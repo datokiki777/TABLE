bb8a14566b6952ae452812d99c97dbd860b49e18
Davit <mamulashvili1985@gmail.com>
2026-03-04 20:46:47 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "client-totals-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./style.css"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
